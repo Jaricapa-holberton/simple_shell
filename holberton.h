9a4cdb04ede7eb0d23cf79444e349ccc30a6ef3b
Jaricapa-holberton <2241@holbertonschool.com>
2021-04-11 09:36:42 -0500

    separate functions in new files

--- holberton.h
+++ holberton.h
@@ -23,7 +23,7 @@ int shell_execute(char **args);
 int shell_cd(char **args);
 int shell_help(char **args);
 int shell_exit(char **args);
-
+int shell_num_builtins(void);
 /* Environment variables and Function´s */
 char *_getenv(const char *name);
 
--- shell_execute.c
+++ shell_execute.c
@@ -1,69 +1,7 @@
 #include "holberton.h"
-/* Function Declarations for builtin shell commands: */
-int shell_cd(char **args);
-int shell_help(char **args);
-int shell_exit(char **args);
 /* List of builtin commands, followed by their corresponding functions. */
 char *builtin_str[] = {"cd", "help", "exit"};
 int (*builtin_func[]) (char **) = {&shell_cd, &shell_help, &shell_exit};
-/**
- * shell_num_builtins - count the amount of builtins avalibe
- * Return: return the number of builtins
- */
-int shell_num_builtins(void)
-{
-	return (sizeof(builtin_str) / sizeof(char *));
-}
-/* Builtin function implementations. */
-/**
- * shell_cd - change directory
- * @args: array of strings with the arguments
- * Return: return 1 if success or exit if fail
- */
-int shell_cd(char **args)
-{
-	if (args[1] == NULL)
-	{
-		perror("expected argument to \"cd\"\n");
-	}
-	else
-	{
-		if (chdir(args[1]) != 0)
-		{
-			perror("lsh");
-		}
-	}
-	return (1);
-}
-/**
- * shell_help - display a help text of the shell
- * @args: array of strings with the arguments
- * Return: return 1 if success or exit if fail
- */
-int shell_help(char **args)
-{
-	int i;
-	*args = *args;
-	_puts("Jaime Aricapa and Frank Grijalba\n");
-	_puts("Type program names and arguments, and hit enter.\n");
-	_puts("The following are built in:\n");
-	for (i = 0; i < shell_num_builtins(); i++)
-	{
-		_puts(builtin_str[i]);
-		_puts(" \n");
-	}
-	_puts("Use the man command for information on other programs.\n");
-	return (1);
-}
-/**
- * shell_exit - exit of the shell
- * @args: array of strings with the arguments
- * Return: return 1 if success or exit if fail
- */
-int shell_exit(char **args)
-{
-	return (0);
-}
 /**
  * shell_execute - execute the comands inserted from input
  * @args: array of strings with the arguments
@@ -78,14 +16,10 @@ int shell_execute(char **args)
 
 	if (args[0] == NULL)
 	{
-		/* An empty command was entered.*/
 		return (1);
 	}
-	/* Trae el contenido de el entorno PATH */
 	path = _getenv("PATH");
-	/*Split para cada ":" de las rutas de entorno */
 	environs = shell_split_line(path);
-	/* concatenate the path routes with the comands */
 	for (i = 0; i < _strlen(*environs); i++)
 	{
 		pathcat1 = str_concat(environs[i], "/");
